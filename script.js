090d1b950703fbd0e4f2a58559814d18761f33a6
BoesLee <stevebouchery@gmail.com>
2024-03-18 02:16:25 +0100

    Clean script.js

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
-// Create array 
 const options = ["rock", "paper", "scissors"];
 
-// Generate computerSelection
 function getComputerChoice() {
     const min = 0;
     const max = (options.length)-1;
+    // A Proper Random Function according to https://www.w3schools.com/JS/js_random.asp.
     let computerSelection = Math.floor(Math.random() * (max - min + 1) ) + min;
     return computerSelection;
 }
 
-// Prompt playerSelection
 function getPlayerChoice() {
-    // optional: look for better way to prompt, for learning reasons
+    // optional: look for better way to prompt, for learning reasons.
     let playerSelection = prompt(`Select ${options[0]}, ${options[1]}, ${options[2]}`);
     playerSelection = playerSelection.toLowerCase();
-    // REPEAT-UNTIL PlayerSelection included in Array
     if (options.includes(playerSelection)) {
+        // The array's index, instead of the word, makes math possible in the playgame() function.
         return options.indexOf(playerSelection);   
     }
     return getPlayerChoice();
 }
 
-// Get results
 function playGame() {
     if(playerSelection - computerSelection == 0) {
         alert("Draw!");
@@ -34,9 +31,8 @@ function playGame() {
     }
 }
 
-// Prompt for rounds
+// optional: improve readability / make it a function.
 let rounds = prompt("How many rounds?");
-// Play the game
 let computerSelection;
 let playerSelection;
 for(let i = rounds; i > 0; i--) {
@@ -45,4 +41,4 @@ for(let i = rounds; i > 0; i--) {
     playGame();
 }
 
-// optional: include scoreboard
+// optional: include scoreboard.
